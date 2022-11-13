d4227ad56769dd2557dff09ca31f65d4c845d35c
Xiangdong Zeng <xdzeng96@gmail.com>
2022-11-13 21:18:29 +0800

    add build script

---
+++ build.sh
@@ -0,0 +1,15 @@
+datetime=$(date +"%Y-%m-%d %H:%M:%S")
+sed -i -E "s/(^ +Version .+)/\1 (dev $datetime)/g" src/SmileySans.ttx
+
+mkdir build
+fontmake \
+    -g src/SmileySansX.glyphspackage \
+    --output-dir build \
+    --master-dir {tmp} \
+    --timing
+
+for ext in otf ttf
+do
+    ttx -m build/SmileySans-Oblique.$ext -o build/SmileySans-Oblique.$ext src/SmileySans.ttx
+    python -c "from fontTools.ttLib import woff2; woff2.compress('build/SmileySans-Oblique.$ext', 'build/SmileySans-Oblique.$ext.woff2')"
+done
